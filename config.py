c7116c8439d41d18f1772d7af197fc937ace01e8
Kirito <hamzakashifkhanzada@gmail.com>
2025-05-05 20:17:48 +0500

    Added config to gitignore and stuff

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:kirito500@localhost/cr_medsync'
+    
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_NAME = os.environ.get('DB_NAME')
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
